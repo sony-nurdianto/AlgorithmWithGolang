834675e531de771282766d9ee808326ff4dc0481
sony-nurdianto <sony@desatech.id>
2025-02-21 14:21:50 +0700

    list and playground

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sony-nurdianto/AlgorithmWithGolang
 
 go 1.23.0
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.0 // indirect
+)
--- playground/main.go
+++ playground/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sort"
-	"sync"
 )
 
 func Generator(done <-chan struct{}, num [3][3]int) <-chan [3]int {
@@ -44,38 +43,52 @@ func Indexing(done <-chan struct{}, num <-chan [3]int) <-chan int {
 	return stream
 }
 
-func Grouping(done <-chan struct{}, numStream <-chan int) <-chan []int {
-	var wg sync.WaitGroup
-	out := make(chan []int)
-
-	var arr []int
-	for n := range numStream {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
+func OrDone(done <-chan struct{}, c <-chan int) <-chan int {
+	valueStream := make(chan int)
+	go func() {
+		defer close(valueStream)
+		for {
 			select {
 			case <-done:
 				return
-			default:
-				arr = append(arr, i)
+			case v, ok := <-c:
+				if !ok {
+					return
+				}
+				select {
+				case valueStream <- v:
+				case <-done:
+				}
 			}
-		}(n)
-	}
+		}
+	}()
+
+	return valueStream
+}
+
+func Grouping(done <-chan struct{}, numStream <-chan int) <-chan []int {
+	out := make(chan []int)
 
 	go func() {
-		wg.Wait()
-		out <- arr
-		close(out)
+		defer close(out)
+
+		var arr []int
+
+		for n := range numStream {
+			arr = append(arr, n)
+		}
+
+		sort.Ints(arr)
+
+		select {
+		case <-done:
+			return
+		case out <- arr:
+		}
 	}()
 
 	return out
 }
-
-type SortIntArr []int
-
-func (a SortIntArr) Len() int           { return len(a) }
-func (a SortIntArr) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a SortIntArr) Less(i, j int) bool { return a[i] < a[j] }
 
 func main() {
 	done := make(chan struct{})
@@ -88,11 +101,5 @@ func main() {
 
 	grouping := Grouping(done, Indexing(done, Generator(done, twoDArray)))
 
-	var arr SortIntArr
-	if data, ok := <-grouping; ok {
-		arr = SortIntArr(data)
-	}
-
-	sort.Sort(arr)
-	fmt.Println(arr)
+	fmt.Println(<-grouping)
 }
